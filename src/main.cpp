96be9a1fb33dc28ebb6921660428804ab76116ab
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-12-15 14:37:37 +0100

    setup cmake compilation

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// #include <algorithm>
+// #include <boost/lambda/lambda.hpp>
+// #include <iostream>
+// #include <iterator>
+
+
+// int main() {
+//   using namespace boost::lambda;
+//   typedef std::istream_iterator<int> in;
+
+//   std::for_each(in(std::cin), in(), std::cout << (_1 * 3) << " ");
+// }
+
+#include <iostream>
+
+int main() {
+  std::cout << "Test\n";
+  return 0;
+}
